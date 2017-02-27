d1c79820190c0aaf6d317ca35d66e06be400eb56
Swanand <MAC@MACs-MacBook-Pro.local>
2017-02-27 14:50:37 +0530

    added settings screen with back button.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,3 +3,25 @@ function doClick(e) {
 }
 
 $.index.open();
+
+$.webView.addEventListener('load', function(e){
+	if($.webView.canGoBack()){
+		$.back.opacity = 1;
+	}else{
+		$.back.opacity = 0.3;
+	}
+	
+	if($.webView.canGoForward()){
+		$.fwd.opacity = 1;
+	}else{
+		$.fwd.opacity = 0.3;
+	}
+});
+
+function goBack(){
+	$.webView.goBack();
+}
+
+function goFwd(){
+	$.webView.goForward();
+}
